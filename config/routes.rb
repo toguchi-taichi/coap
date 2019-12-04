85214be6fc5510122bbf361995a765e73b05082b
渡久地太一 <togtai1992@gmail.com>
2019-12-04 07:50:16 +0000

    coapの初期データ

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'apples/index'
+  get 'apples/show'
+  get 'apples/new'
+  get 'apples/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ spec/models/apple_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Apple, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
